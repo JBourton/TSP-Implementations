bea41cc0262d261f5f083c119333bc1ee15fff11
JBourton <joshua.bourton16@gmail.com>
2024-01-25 16:31:46 +0000

    Reorder files based on skeleton.py

--- skeleton_clone.py
+++ skeleton_clone.py
@@ -268,7 +268,7 @@ print("The codes and tariffs have been read from 'alg_codes_and_tariffs.txt':")
 ############
 ############ END OF SECTOR 5 (IGNORE THIS COMMENT)
 
-my_user_name = "abcd12"
+my_user_name = "xtzv61"
 
 ############ START OF SECTOR 6 (IGNORE THIS COMMENT)
 ############
@@ -281,8 +281,8 @@ my_user_name = "abcd12"
 ############
 ############ END OF SECTOR 6 (IGNORE THIS COMMENT)
 
-my_first_name = ""
-my_last_name = ""
+my_first_name = "Joshua"
+my_last_name = "Bourton"
 
 ############ START OF SECTOR 7 (IGNORE THIS COMMENT)
 ############
